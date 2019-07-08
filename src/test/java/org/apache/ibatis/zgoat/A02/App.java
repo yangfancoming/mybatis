bd6f2363397312ba628ae934451d81f4ced8e0f1
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-07-08 13:05:34 +0800

    增加 测试模块  复习mybatis的简单使用

--- src/test/java/org/apache/ibatis/zgoat/A02/App.java
+++ src/test/java/org/apache/ibatis/zgoat/A02/App.java
@@ -1,5 +1,5 @@
 
-package org.apache.ibatis.zgoat.A01;
+package org.apache.ibatis.zgoat.A02;
 
 import org.apache.ibatis.BaseDataTest;
 import org.apache.ibatis.io.Resources;
@@ -11,20 +11,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.Reader;
+import java.util.List;
 
 
 class App {
 
   private static SqlSessionFactory sqlSessionFactory;
   private static SqlSession sqlSession;
+  private static FooMapper fooMapper;
+
 
   @BeforeEach
   void setUp() throws Exception {
-    try (Reader reader = Resources.getResourceAsReader("org/apache/ibatis/zgoat/A01/mybatis-config.xml")) {
+    try (Reader reader = Resources.getResourceAsReader("org/apache/ibatis/zgoat/A02/mybatis-config.xml")) {
       // 通过 mybatis 全局配置文件  创建  sqlSessionFactory
       sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
       // 通过 sqlSessionFactory 创建 sqlSession
       sqlSession = sqlSessionFactory.openSession(false);
+      fooMapper= sqlSession.getMapper(FooMapper.class);
     }
     // 创建内存数据库 并添加插入测试数据
     BaseDataTest.runScript(sqlSessionFactory.getConfiguration().getEnvironment().getDataSource(), "org/apache/ibatis/zgoat/common/CreateDB.sql");
@@ -35,23 +39,30 @@ class App {
     sqlSession.close();
   }
 
+
 /**
- *  查询后 会发现  lastname=null 的问题 是因为 实体类的 lastname 属性 与 数据库表的 last_name 没有对应上
- *  解决方法：
- *  1. 改sql    select id,firstname,last_name as lastname from foo where id = #{id}
- *  2. 全局 mybatis-config.xml <configuration> <settings>  增加  <setting name="mapUnderscoreToCamelCase" value="true"/>
+
+ 两处绑定：
+ 1. 文件绑定：xml 中的 <mapper namespace="org.apache.ibatis.zgoat.A02.FooMapper">  必须指定 接口类全路径
+ 1. 方法绑定：xml 中的 statement id  必须与  mapper接口类中的 方法名  一致！
 */
   @Test
-  void test() {
-    // 在没有 xxxMapper 接口类的情况下 Foo.xml 中的 <mapper namespace="com.goat.test.none">  是可以随意指定的。
-    Foo foo1 = sqlSession.selectOne("com.goat.test.none.selectById",2);
-    System.out.println(foo1); // id=2, lastname=null, firstname=222
-
-    // 不加 namespace 前缀也可以调用 但是多个xml中一旦有重名的 selectById 将会冲突 所以最好加上 namespace前缀。
-    Foo foo2 = sqlSession.selectOne("selectById",2);
-    System.out.println(foo2 == foo1); // 走缓存
+  void test1() {
+    List<Foo> all = fooMapper.findAll(); // 查库
+    System.out.println(all);
+    int i = fooMapper.deleteById(2);
+    System.out.println(i);
+    Foo foo = fooMapper.selectById(1);
+    System.out.println(foo);
   }
 
-
+  /**
+   *  mapper 接口 与 xml 文件绑定后   其实就是 xml 作为 mapper接口的实现类
+   *  只不过这个实现类  是 由mybatis 创建的代理实现类  （JDK动态代理）
+  */
+  @Test
+  void test2() {
+    System.out.println(fooMapper.getClass()); // class com.sun.proxy.$Proxy13
+  }
 
 }
--- src/test/java/org/apache/ibatis/zgoat/A02/FooMapper.java
+++ src/test/java/org/apache/ibatis/zgoat/A02/FooMapper.java
@@ -1,13 +1,15 @@
 
-package org.apache.ibatis.zgoat.A01;
+package org.apache.ibatis.zgoat.A02;
+
 
 
 import java.util.List;
 
 public interface FooMapper {
 
+  Foo selectById(Integer id);
+
   List<Foo> findAll();
 
   int deleteById(Integer id);
-
 }
