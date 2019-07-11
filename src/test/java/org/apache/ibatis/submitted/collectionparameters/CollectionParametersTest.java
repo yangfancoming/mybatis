e0dd972ed31f85122c1d28c2767f9fc1dbd79dc3
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-07-11 20:50:39 +0800

    学习测试用例   CollectionParametersTest 集合作为sql参数

--- src/test/java/org/apache/ibatis/submitted/collectionparameters/CollectionParametersTest.java
+++ src/test/java/org/apache/ibatis/submitted/collectionparameters/CollectionParametersTest.java
@@ -2,10 +2,7 @@
 package org.apache.ibatis.submitted.collectionparameters;
 
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import org.apache.ibatis.BaseDataTest;
 import org.apache.ibatis.io.Resources;
@@ -33,15 +30,21 @@ class CollectionParametersTest {
     BaseDataTest.runScript(sqlSessionFactory.getConfiguration().getEnvironment().getDataSource(),"org/apache/ibatis/submitted/collectionparameters/CreateDB.sql");
   }
 
+  /**
+   * 集合参数 之
+   * list参数   使用list作为参数
+  */
   @Test
   void shouldGetTwoUsersPassingAList() {
-    ArrayList<Integer> list = new ArrayList<>();
-    list.add(1);
-    list.add(2);
+    List<Integer> list = Arrays.asList(1,2);
     List<User> users = mapper.getUsersFromList(list);
     Assertions.assertEquals(2, users.size());
   }
 
+  /**
+   * 集合参数 之
+   * array参数   使用数组作为参数
+   */
   @Test
   void shouldGetTwoUsersPassingAnArray() {
     Integer[] list = new Integer[2];
@@ -51,6 +54,10 @@ class CollectionParametersTest {
     Assertions.assertEquals(2, users.size());
   }
 
+  /**
+   * 集合参数 之
+   * set参数   使用set作为参数
+   */
   @Test
   void shouldGetTwoUsersPassingACollection() {
     Set<Integer> list = new HashSet<>();
