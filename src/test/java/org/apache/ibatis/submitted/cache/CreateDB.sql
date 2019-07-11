0e7f9eff0aedb041b624cabb87460cea5d084843
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-07-11 16:55:50 +0800

    修改mysql  删表语句

--- src/test/java/org/apache/ibatis/submitted/cache/CreateDB.sql
+++ src/test/java/org/apache/ibatis/submitted/cache/CreateDB.sql
@@ -1,7 +1,7 @@
 
 
-drop table person if exists;
-
+# drop table person if exists;
+drop table if exists person;
 create table person(
 	id int,
 	firstname varchar(20),
@@ -12,8 +12,8 @@ insert into person(id, firstname, lastname) values (1, 'Jane', 'Doe');
 insert into person(id, firstname, lastname) values (2, 'John', 'Smith');
 
 
-drop table foo if exists;
-
+# drop table foo if exists;
+drop table if exists foo;
 create table foo(
                      id int,
                      firstname varchar(20),
