6e18c216cb8b5094aff1166e28687bf83eee1bc3
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-07-11 09:15:48 +0800

    学习 别名注册 测试示例

--- src/main/java/org/apache/ibatis/type/TypeAliasRegistry.java
+++ src/main/java/org/apache/ibatis/type/TypeAliasRegistry.java
@@ -18,14 +18,28 @@ import java.util.Set;
 import org.apache.ibatis.io.ResolverUtil;
 import org.apache.ibatis.io.Resources;
 
-
+/**
+ TypeAliasRegistry（类型别名注册器）
+ 在类型别名注册器类TypeAliasRegistry的无参构造器中进行了大量的基础类型别名的注册（设置），涉及到的有：
+ 　　　　1.字符串类型（别名类似string）
+ 　　　　2.基本类型包装器类型及其数组类型（别名类似byte、byte[]）
+ 　　　　3.基本类型及其数组类型（别名类似_byte、_byte[]）
+ 　　　　4.日期类型及其数组类型（别名类似date、date[]）
+ 　　　　5.大数字类型及其数组类型（别名类似bigdecimal、bigdecimal[]）
+ 　　　　6.Object类型及其数组类型（别名类似object、object[]）
+ 　　　　7.集合类型（别名类似collection、map、list、hsahmap、arraylist、iterator）
+ 　　　　8.ResultSet结果集类型（别名为ResultSet）
+ 　注意：这并不是全部的MyBatis内置的类型别名，还有一部分类型别名是在创建Configuration实例的时候在其无参构造器中进行注册的，这里暂不介绍。
+*/
 public class TypeAliasRegistry {
 
   private final Map<String, Class<?>> typeAliases = new HashMap<>();
 
+  //构造函数里注册系统内置的类型别名
   public TypeAliasRegistry() {
+    //字符串类型
     registerAlias("string", String.class);
-
+    //基本包装类型
     registerAlias("byte", Byte.class);
     registerAlias("long", Long.class);
     registerAlias("short", Short.class);
@@ -34,7 +48,7 @@ public class TypeAliasRegistry {
     registerAlias("double", Double.class);
     registerAlias("float", Float.class);
     registerAlias("boolean", Boolean.class);
-
+    //基本数组包装类型
     registerAlias("byte[]", Byte[].class);
     registerAlias("long[]", Long[].class);
     registerAlias("short[]", Short[].class);
@@ -43,7 +57,7 @@ public class TypeAliasRegistry {
     registerAlias("double[]", Double[].class);
     registerAlias("float[]", Float[].class);
     registerAlias("boolean[]", Boolean[].class);
-
+    //加个下划线，就变成了基本类型
     registerAlias("_byte", byte.class);
     registerAlias("_long", long.class);
     registerAlias("_short", short.class);
@@ -52,7 +66,7 @@ public class TypeAliasRegistry {
     registerAlias("_double", double.class);
     registerAlias("_float", float.class);
     registerAlias("_boolean", boolean.class);
-
+    //加个下划线，就变成了基本数组类型
     registerAlias("_byte[]", byte[].class);
     registerAlias("_long[]", long[].class);
     registerAlias("_short[]", short[].class);
@@ -61,19 +75,19 @@ public class TypeAliasRegistry {
     registerAlias("_double[]", double[].class);
     registerAlias("_float[]", float[].class);
     registerAlias("_boolean[]", boolean[].class);
-
+    //日期数字型
     registerAlias("date", Date.class);
     registerAlias("decimal", BigDecimal.class);
     registerAlias("bigdecimal", BigDecimal.class);
     registerAlias("biginteger", BigInteger.class);
     registerAlias("object", Object.class);
-
+    //集合型
     registerAlias("date[]", Date[].class);
     registerAlias("decimal[]", BigDecimal[].class);
     registerAlias("bigdecimal[]", BigDecimal[].class);
     registerAlias("biginteger[]", BigInteger[].class);
     registerAlias("object[]", Object[].class);
-
+    //还有个ResultSet型
     registerAlias("map", Map.class);
     registerAlias("hashmap", HashMap.class);
     registerAlias("list", List.class);
@@ -84,6 +98,7 @@ public class TypeAliasRegistry {
     registerAlias("ResultSet", ResultSet.class);
   }
 
+  //取出 已经注册的类型别名
   @SuppressWarnings("unchecked")
   // throws class cast exception as well if types cannot be assigned
   public <T> Class<T> resolveAlias(String string) {
@@ -92,11 +107,18 @@ public class TypeAliasRegistry {
         return null;
       }
       // issue #748
+      /**
+       先转成小写再解析 这里转个小写也有bug？见748号bug(在google code上)
+       https://code.google.com/p/mybatis/issues
+       比如如果本地语言是Turkish，那i转成大写就不是I了，而是另外一个字符（İ）。这样土耳其的机器就用不了mybatis了！这是一个很大的bug，但是基本上每个人都会犯......
+      */
       String key = string.toLowerCase(Locale.ENGLISH);
       Class<T> value;
+      //原理就很简单了，从HashMap里找对应的键值，找到则返回类型别名对应的Class
       if (typeAliases.containsKey(key)) {
         value = (Class<T>) typeAliases.get(key);
       } else {
+        //找不到，再试着将String直接转成Class(这样怪不得我们也可以直接用java.lang.Integer的方式定义，也可以就int这么定义)
         value = (Class<T>) Resources.classForName(string);
       }
       return value;
@@ -108,6 +130,7 @@ public class TypeAliasRegistry {
   public void registerAliases(String packageName) {
     registerAliases(packageName, Object.class);
   }
+
 
   public void registerAliases(String packageName, Class<?> superType) {
     ResolverUtil<Class<?>> resolverUtil = new ResolverUtil<>();
@@ -131,12 +154,17 @@ public class TypeAliasRegistry {
     registerAlias(alias, type);
   }
 
+  //注册类型别名
   public void registerAlias(String alias, Class<?> value) {
     if (alias == null) {
       throw new TypeException("The parameter alias cannot be null");
     }
     // issue #748
     String key = alias.toLowerCase(Locale.ENGLISH);
+    /**
+     如果已经存在key了，且value和之前不一致，报错
+     这里逻辑略显复杂，感觉没必要，一个key对一个value呗，存在key直接报错不就得了
+    */
     if (typeAliases.containsKey(key) && typeAliases.get(key) != null && !typeAliases.get(key).equals(value)) {
       throw new TypeException("The alias '" + alias + "' is already mapped to the value '" + typeAliases.get(key).getName() + "'.");
     }
--- src/test/java/org/apache/ibatis/type/TypeAliasRegistryTest.java
+++ src/test/java/org/apache/ibatis/type/TypeAliasRegistryTest.java
@@ -1,56 +1,59 @@
 
 package org.apache.ibatis.type;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import org.junit.jupiter.api.Test;
-
 import java.math.BigDecimal;
+import static org.junit.jupiter.api.Assertions.*;
 
 class TypeAliasRegistryTest {
 
+  TypeAliasRegistry typeAliasRegistry = new TypeAliasRegistry();
+
+  //  注册别名 map 和 取出别名 map 操作
   @Test
   void shouldRegisterAndResolveTypeAlias() {
-    TypeAliasRegistry typeAliasRegistry = new TypeAliasRegistry();
-
+    // 注册操作
     typeAliasRegistry.registerAlias("rich", "org.apache.ibatis.domain.misc.RichType");
-
+    // 取出操作
+    Class<Object> rich = typeAliasRegistry.resolveAlias("rich");
+    System.out.println(rich.getName());
     assertEquals("org.apache.ibatis.domain.misc.RichType", typeAliasRegistry.resolveAlias("rich").getName());
   }
 
   @Test
   void shouldFetchArrayType() {
-    TypeAliasRegistry typeAliasRegistry = new TypeAliasRegistry();
+    Class<Object> objectClass = typeAliasRegistry.resolveAlias("byte[]");
+    System.out.println(objectClass); // class [Ljava.lang.Byte;
     assertEquals(Byte[].class, typeAliasRegistry.resolveAlias("byte[]"));
   }
 
   @Test
   void shouldBeAbleToRegisterSameAliasWithSameTypeAgain() {
-    TypeAliasRegistry typeAliasRegistry = new TypeAliasRegistry();
+    // 能够再次注册相同类型的别名     其中 alias 参数 无论大小写进源码后 都会被转成小写存储
     typeAliasRegistry.registerAlias("String", String.class);
     typeAliasRegistry.registerAlias("string", String.class);
   }
 
   @Test
   void shouldNotBeAbleToRegisterSameAliasWithDifferentType() {
-    TypeAliasRegistry typeAliasRegistry = new TypeAliasRegistry();
+    //  别名相同 但对应的类型不同 则不能够注册
     assertThrows(TypeException.class, () -> typeAliasRegistry.registerAlias("string", BigDecimal.class));
   }
 
   @Test
   void shouldBeAbleToRegisterAliasWithNullType() {
-    TypeAliasRegistry typeAliasRegistry = new TypeAliasRegistry();
+    //  别名对应类型为null的  也可以注册
     typeAliasRegistry.registerAlias("foo", (Class<?>) null);
+    System.out.println(typeAliasRegistry.resolveAlias("foo"));// null
     assertNull(typeAliasRegistry.resolveAlias("foo"));
   }
 
   @Test
   void shouldBeAbleToRegisterNewTypeIfRegisteredTypeIsNull() {
-    TypeAliasRegistry typeAliasRegistry = new TypeAliasRegistry();
+    // 如果已经注册的 别名对应类型为null 则可以再次注册新的类型
     typeAliasRegistry.registerAlias("foo", (Class<?>) null);
     typeAliasRegistry.registerAlias("foo", String.class);
+    System.out.println(typeAliasRegistry.resolveAlias("foo"));//  class java.lang.String
   }
 
 }
