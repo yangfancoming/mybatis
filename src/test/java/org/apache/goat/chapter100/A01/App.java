bf3517f889adb7a67f43445f9489ce7e869bce67
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-08-21 22:06:06 +0800

    增加  A01 注释

--- src/test/java/org/apache/goat/chapter100/A01/App.java
+++ src/test/java/org/apache/goat/chapter100/A01/App.java
@@ -9,7 +9,10 @@ public class App extends MyBaseDataTest {
 
   public static final String XMLPATH = "org/apache/goat/chapter100/A01/mybatis-config.xml";
 
-  /**
+  /** javaBean中的属性 与 数据库表字段 不对应情况的 两种解决方式：  */
+
+
+  /**  解决方式一： 开启 全局 mapUnderscoreToCamelCase  配置
    * 运行结果可以看到 Employee{id=1, lastName='tom', email='tom@qq.com', gender='0'}
    * 即 开启了  <setting name="mapUnderscoreToCamelCase" value="true"/>  全局配置
    * 导致 javabean中的属性 lastName  数据库表中的字段 last_name 则 last_name 会被映射为 lastName 可以获取到该属性
@@ -22,6 +25,15 @@ public class App extends MyBaseDataTest {
   }
 
 
-
+  /** 解决方式二： 局部xml中的sql 查询字典使用as映射
+   * 注释掉 全局 setting 配置
+   * 运行结果仍然 可以看到  Employee{id=1, lastName='tom', email='tom@qq.com', gender='0'}
+  */
+  @Test
+  void InputStream() throws Exception {
+    setUpByInputStream(XMLPATH);
+    Employee o = sqlSession.selectOne("com.goat.test.namespace.getEmpById2", 1);
+    System.out.println(o.toString());
+  }
 
 }
