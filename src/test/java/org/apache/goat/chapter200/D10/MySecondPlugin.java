461d2f4134a0154622479e9361f413d7676c4cb3
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-10-28 09:10:57 +0800

    增加 mybatis 插件  示例

---
+++ src/test/java/org/apache/goat/chapter200/D10/MySecondPlugin.java
@@ -0,0 +1,32 @@
+package org.apache.goat.chapter200.D10;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.*;
+
+import java.util.Properties;
+
+@Intercepts(
+		{
+			@Signature(type= StatementHandler.class,method="parameterize",args=java.sql.Statement.class)
+		})
+public class MySecondPlugin implements Interceptor {
+
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		System.out.println("MySecondPlugin...intercept:"+invocation.getMethod());
+		return invocation.proceed();
+	}
+
+	@Override
+	public Object plugin(Object target) {
+		// TODO Auto-generated method stub
+		System.out.println("MySecondPlugin...plugin:"+target);
+		return Plugin.wrap(target, this);
+	}
+
+	@Override
+	public void setProperties(Properties properties) {
+		// TODO Auto-generated method stub
+	}
+
+}
