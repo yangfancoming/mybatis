aa2e391d4177a0e2a03a6af5706c0ce50368d392
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-11-01 12:47:00 +0800

    代码格式优化

--- src/main/java/org/apache/ibatis/builder/InitializingObject.java
+++ src/main/java/org/apache/ibatis/builder/InitializingObject.java
@@ -3,6 +3,7 @@ package org.apache.ibatis.builder;
 
 /**
  * Interface that indicate to provide a initialization method.
+ * 指示提供初始化方法的接口
  * @since 3.4.2
  */
 public interface InitializingObject {
@@ -10,6 +11,7 @@ public interface InitializingObject {
   /**
    * Initialize a instance.
    * This method will be invoked after it has set all properties.
+   * 此方法将在设置完所有属性后调用
    * @throws Exception in the event of misconfiguration (such as failure to set an essential property) or if initialization fails
    */
   void initialize() throws Exception;
