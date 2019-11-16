9ec5fb1bab92df8b53af97a0eb778caaf3ec816d
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-11-16 14:52:34 +0800

    增加注释

--- src/test/java/org/apache/goat/chapter500/PropertyParser/App.java
+++ src/test/java/org/apache/goat/chapter500/PropertyParser/App.java
@@ -3,6 +3,10 @@ package org.apache.goat.chapter500.PropertyParser;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 import java.util.Properties;
 
 
@@ -41,5 +45,24 @@ public class App {
     props.setProperty("key", "value");
     String parse = PropertyParser.parse("${key1:aaaa}", props);
     Assert.assertEquals("aaaa" ,parse);
+  }
+
+  @Test
+  public void test4() throws IOException {
+    //创建数据报套接字对象，绑定端口号为6000
+    DatagramSocket ds = new DatagramSocket(5002);
+    //构建数据包接收数据：
+    //创建字节数组
+    byte[] buf = new byte[100];
+    //创建数据包对象，它的长度不能超过数组的长度，我们把它设为100
+    DatagramPacket dp = new DatagramPacket(buf, 100);
+    //接收数据
+    ds.receive(dp);
+    //打印数据
+    //getLength方法返回实际接收数；getData方法返回数据，返回格式为字节数组
+    System.out.println(new String(buf, 0, dp.getLength()));
+    //关闭数据报套接字
+    ds.close();
+
   }
 }
