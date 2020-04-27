6d8b683bb4fe66315fa546671626aebb9f2aeda5
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-04-27 13:36:30 +0800

    整理测试用例

--- src/test/java/org/apache/goat/chapter100/A/A030/App.java
+++ src/test/java/org/apache/goat/chapter100/A/A030/App.java
@@ -9,13 +9,20 @@ import org.junit.jupiter.api.Test;
 */
 class App extends MyBaseDataTest {
 
-
   /**
    * 别名方式一：  为单个类 起别名
    * 就是 全局xml 中配置  <typeAlias type="org.apache.goat.common.Foo" alias="Foo"/> 后 ，局部xml 中就可以直接写成  resultType="Foo"
   */
   @Test
   void test1() throws Exception {
-    setUpByReaderNoOpen("org/apache/goat/chapter100/A/A030/mybatis-config.xml");
+    setUpByReaderNoOpen("org/apache/goat/chapter100/A/A030/typeAlias1.xml");
+  }
+
+  /**
+   * 别名方式三：   批量起别名
+   */
+  @Test
+  void test2() throws Exception {
+    setUpByReaderNoOpen("org/apache/goat/chapter100/A/A030/typeAlias3.xml");
   }
 }
