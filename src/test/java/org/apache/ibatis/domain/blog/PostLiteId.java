8fe2088be7e883aaa4f2621d300b7d6b3f98523e
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-05-21 22:38:02 +0800

    代码格式优化

--- src/test/java/org/apache/ibatis/domain/blog/PostLiteId.java
+++ src/test/java/org/apache/ibatis/domain/blog/PostLiteId.java
@@ -2,44 +2,45 @@
 package org.apache.ibatis.domain.blog;
 
 public class PostLiteId {
-    private int id;
 
-    public PostLiteId() {
+  private int id;
 
+  public PostLiteId() {
+
+  }
+
+  public PostLiteId(int aId) {
+    id = aId;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
 
-    public void setId(int id) {
-      this.id = id;
+    final PostLiteId that = (PostLiteId) o;
+
+    if (id != that.id) {
+      return false;
     }
 
-    public PostLiteId(int aId) {
-        id = aId;
-    }
+    return true;
+  }
 
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        final PostLiteId that = (PostLiteId) o;
-
-        if (id != that.id) {
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return id;
-    }
+  @Override
+  public int hashCode() {
+    return id;
+  }
 }
