984c76a479686a00c9dc0f67b9a00543eed8b9e0
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-05-22 08:53:26 +0800

    增加调试日志

--- src/test/java/org/apache/ibatis/submitted/named_constructor_args/User.java
+++ src/test/java/org/apache/ibatis/submitted/named_constructor_args/User.java
@@ -11,22 +11,26 @@ public class User {
 
   public User(@Param("id") String id) {
     super();
+    System.out.println("1个参数构造方法执行");
     this.id = Integer.valueOf(id);
   }
 
   public User(Integer userId, @Param("name") String userName) {
     super();
+    System.out.println("2个参数构造方法执行");
     this.id = userId;
     this.name = userName;
   }
 
   public User(@Param("id") int id, @Param("name") String name, @Param("team") String team) {
     super();
+    System.out.println("3个参数构造方法执行 int");
     // NOP constructor to make sure MyBatis performs strict type matching.
   }
 
   public User(@Param("id") Integer id, @Param("name") String name, @Param("team") String team) {
     super();
+    System.out.println("3个参数构造方法执行 Integer");
     this.id = id;
     this.name = name;
     this.team = Long.valueOf(team);
