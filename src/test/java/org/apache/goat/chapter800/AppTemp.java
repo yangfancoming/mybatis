8e47c885e33cc1f7362dc8c2a2fc61a63bded5ba
yangfan <34465021+jwfl724168@users.noreply.github.com>
2020-08-12 23:38:54 +0800

    增加测试类

--- src/test/java/org/apache/goat/chapter800/AppTemp.java
+++ src/test/java/org/apache/goat/chapter800/AppTemp.java
@@ -17,16 +17,26 @@ import java.util.Date;
 public class AppTemp {
 
   @Test
-  public void test2(){
+  public void test1(){
     BigDecimal temp1 = new BigDecimal(0);
     BigDecimal temp2 = new BigDecimal(0);
     BigDecimal subtract = temp1.subtract(temp2);
     System.out.println(subtract);
   }
+
   @Test
-  public void test3() throws ParseException {
+  public void test2() throws ParseException {
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
     Date dateStr = sdf.parse("1970-01-01");
     System.out.println(dateStr);
+
+  }
+
+  @Test
+  public void test3() throws ParseException {
+    Date dateStr = new Date();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    String format = sdf.format(dateStr);
+    System.out.println(format.length());
   }
 }
