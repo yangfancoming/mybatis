03781fa22a6b280e4bdcaf2658680175516d50f7
yangfan <34465021+jwfl724168@users.noreply.github.com>
2021-05-30 15:35:05 +0800

    学习 mybatis 解析xml的两种方式

--- src/test/java/org/apache/goat/chapter100/A/A000/App.java
+++ src/test/java/org/apache/goat/chapter100/A/A000/App.java
@@ -1,8 +1,13 @@
 package org.apache.goat.chapter100.A.A000;
 
 import org.apache.common.MyBaseDataTest;
-import org.apache.goat.common.model.Foo;
+import org.apache.ibatis.datasource.unpooled.UnpooledDataSource;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
 
 /**
  # mybatis  读取 xml 的三种方式
@@ -16,25 +21,26 @@ import org.junit.jupiter.api.Test;
 class App extends MyBaseDataTest {
 
   public static final String XMLPATH = "org/apache/goat/chapter100/A/A000/mybatis-config.xml";
-  public static final String DBSQL = "org/apache/goat/common/CreateDB.sql";
 
   /**  1、通过读取字符流（Reader）的方式构件SqlSessionFactory */
   @Test
   void Reader() throws Exception {
-    setUpByReader(XMLPATH,DBSQL);
-    Foo foo1 = sqlSession.selectOne("com.goat.test.namespace.selectById",2);
-    System.out.println(foo1); //
+    setUpByReader(XMLPATH);
+    common();
   }
 
   /**  2、通过字节流（InputStream）的方式构件SqlSessionFacotry */
   @Test
   void InputStream() throws Exception {
-    setUpByInputStream(XMLPATH,DBSQL);
-    Foo foo1 = sqlSession.selectOne("com.goat.test.namespace.selectById",1);
-    System.out.println(foo1); //
+    setUpByInputStream(XMLPATH);
+    common();
   }
 
-  /**  3、通过Configuration对象构建SqlSessionFactory  */
-
-
+  void common(){
+    Configuration configuration = sqlSessionFactory.getConfiguration();
+    Environment environment = configuration.getEnvironment();
+    DataSource dataSource = environment.getDataSource();
+    Assert.assertTrue(dataSource instanceof UnpooledDataSource);
+    Assert.assertEquals("development",environment.getId());
+  }
 }
